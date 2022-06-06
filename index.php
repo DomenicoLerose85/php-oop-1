8d7f469c27fbc8a9c898f875192524cfb69226f4
DomenicoLerose85 <domenicolerose85@libero.it>
2022-06-06 15:15:15 +0200

    creation_of_class_variables_&_method

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
+class Movies
+{
+    // variables
+    public $genre;
+    public $year;
+    public $name;
 
-
-
-?>
+    // method (getter)
+    function get_name($_name)
+    {
+        return $this->name;
+    }
+}

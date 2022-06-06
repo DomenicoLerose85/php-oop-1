e7db532a56395e7e782dcc61197e29db1f2783a1
DomenicoLerose85 <domenicolerose85@libero.it>
2022-06-06 16:33:08 +0200

    end_of_excercise

--- index.php
+++ index.php
@@ -8,20 +8,34 @@ class Movies
 
 
     // construct
-    function __construct($name, $genre)
+    function __construct($_name, $_genre)
     {
-        $this->name = $name;
-        $this->genre = $genre;
+        $this->name = $_name;
+        $this->genre = $_genre;
     }
 
     // method (getter)
-    function get_name($name)
+    function get_name()
     {
         return $this->name;
+    }
+
+    function get_genre()
+    {
+        return $this->genre;
     }
 }
 
 // new object
-$film1 = new Movies('Over the Top', 'Action');
-echo
-// var_dump($film1);
+$film1 = new Movies('Is not a country for hold men', 'Action');
+$film2 = new Movies('The neon demons', 'Horror');
+// $film3 = new Movies ('Unglourios Bastards', 'Action-History', )
+
+echo $film1->get_name();
+echo '<br>';
+echo $film1->get_genre();
+echo '<br>';
+echo $film2->get_name();
+echo '<br>';
+echo $film2->get_genre();
+echo '<br>';
